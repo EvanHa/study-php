9e22900a3abb1b797213b46a6639d9d37c49c9b2
evanha <bc.0x4861@gmail.com>
2021-02-06 10:11:38 +0900

    [Add] data type

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            $a = 1;
+            var_dump($a);
+            
+            $b = 2.2;
+            var_dump($b);
+            
+            $c = 'hello world';
+            var_dump($c);
+            
+            $d = true;
+            var_dump($d);
+        ?>
+    </body>
+</html>
