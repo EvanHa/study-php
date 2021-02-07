71c0c475db2f456757413765473d0ed76155d288
evanha <bc.0x4861@gmail.com>
2021-02-07 22:47:57 +0900

    [Add] 함수 적용

--- src/index.php
+++ src/index.php
@@ -1,3 +1,33 @@
+<?php
+    function print_title() {
+        if(isset($_GET['id'])) {
+            echo $_GET['id'];
+        } else {
+            echo 'Welcome';
+        }
+    }
+    
+    function print_description() {
+        if (isset($_GET['id'])) {
+            echo file_get_contents("data/".$_GET['id']);
+        } else {
+            echo "Hello, PHP";
+        }
+    }
+    
+    function print_list() {
+        $list = scandir('./data');
+    
+        $i = 0;
+        while($i < count($list)) {
+            if ($list[$i] != '.' && $list[$i] != '..') {
+                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+            $i = $i + 1;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,28 +37,16 @@
 <h1>WEB</h1>
 <ol>
     <?php
-        $list = scandir('./data');
-        
-        $i = 0;
-        while($i < count($list)) {
-            if ($list[$i] != '.' && $list[$i] != '..') {
-                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            }
-            $i = $i + 1;
-        }
+        print_list();
     ?>
 </ol>
 <h2>
     <?php
-        if(isset($_GET['id'])) {
-            echo $_GET['id'];
-        } else {
-            echo 'Welcome';
-        }
+        print_title();
     ?>
 </h2>
 <?php
-    echo file_get_contents("data/".$_GET['id']);
+    print_description();
 ?>
 
 </body>
